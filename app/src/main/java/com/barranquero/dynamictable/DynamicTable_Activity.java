ed58b01142da29477c76c8737b2d980e7fedb89b
José Antonio Barranquero <joseantbarranquero@gmail.com>
2016-10-17 12:02:29 +0200

    Finalised project

--- app/src/main/java/com/barranquero/dynamictable/DynamicTable_Activity.java
+++ app/src/main/java/com/barranquero/dynamictable/DynamicTable_Activity.java
@@ -20,8 +20,11 @@ public class DynamicTable_Activity extends AppCompatActivity {
         mTblHeader = (TableLayout)findViewById(R.id.tblHeader);
         mTblBody = (TableLayout)findViewById(R.id.tblBody);
 
+        // Setting up the appearance of the ID column
         mTbrLayoutId = new TableRow.LayoutParams(150, TableRow.LayoutParams.WRAP_CONTENT);
         mTbrLayoutId.setMargins(0,10,0,10);
+
+        // Setting up the appearance of the Name column
         mTbrLayoutName = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
 
         createHeader();
@@ -38,12 +41,14 @@ public class DynamicTable_Activity extends AppCompatActivity {
         TextView txvId = new TextView(this);
         txvId.setText(R.string.txvId);
         txvId.setLayoutParams(mTbrLayoutId);
+        txvId.setBackgroundResource(R.drawable.shape_header);
         tbrHeader.addView(txvId);
 
         // We create the Name column
         TextView txvName = new TextView(this);
         txvName.setText(R.string.txvName);
         txvName.setLayoutParams(mTbrLayoutName);
+        txvName.setBackgroundResource(R.drawable.shape_header);
         tbrHeader.addView(txvName);
 
         // We add both columns (the headers) to the Header TableLayout
@@ -54,23 +59,35 @@ public class DynamicTable_Activity extends AppCompatActivity {
      * Method which sets the Table Body
      */
     private void createBody() {
-        TableRow tbrBody = new TableRow(this);
+        TableRow tbrBody;
 
-        TextView txvId = new TextView(this);
-        TextView txvName = new TextView(this);
+        TextView txvId;
+        TextView txvName;
 
+        // We get the name list from the array declared in the file 'strings.xml'
         String[] nameList = getResources().getStringArray(R.array.nameList);
 
         for (int i = 0; i < nameList.length; i++) {
-            txvId.setText(i);
-            txvId.setLayoutParams(mTbrLayoutId);
+            tbrBody = new TableRow(this);   // We create a row for each name
+
+            // For every item, we create two TextViews: one for the ID, one for the name
+            txvId = new TextView(this);
+            txvName = new TextView(this);
+
+            txvId.setText(" "+ (i + 1) +" ");
+            txvId.setLayoutParams(mTbrLayoutId);    // We apply the LayoutParams to the TextView
+            txvId.setBackgroundResource(R.drawable.shape_body);     // We apply the shape we created
+
             txvName.setText(nameList[i]);
             txvName.setLayoutParams(mTbrLayoutName);
+            txvName.setBackgroundResource(R.drawable.shape_body);
 
+            // We add both TextViews to the TableRow
             tbrBody.addView(txvId);
             tbrBody.addView(txvName);
 
-            mTblHeader.addView(tbrBody);
+            // We add the TableRow to the TableLayout
+            mTblBody.addView(tbrBody);
         }
     }
 }
